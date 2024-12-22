32151a12a456a35dac2a3f84d4f5a78393bf5c58
Rajesh Benjwal <rbenjwal@gmail.com>
2024-12-22 14:15:34 +0530

    Bump version to 1.0.1

--- wp-content/plugins/live-messages/live-messages.php
+++ wp-content/plugins/live-messages/live-messages.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Live Messages
 Description: Display live updating short messages like tweets
-Version: 1.0.0
+Version: 1.0.1
 Author: Rajesh Benjwal
 Author URI: https://tantrakul.org
 GitHub Plugin URI: rajroshi/live-messages
@@ -30,7 +30,7 @@ $updateChecker->setAuthentication('your-github-token'); // Optional: for private
 if (!defined('ABSPATH')) exit;
 
 // Define plugin constants
-define('LIVE_MESSAGES_VERSION', '1.0.0');
+define('LIVE_MESSAGES_VERSION', '1.0.1');
 define('LIVE_MESSAGES_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('LIVE_MESSAGES_PLUGIN_URL', plugin_dir_url(__FILE__));
 
